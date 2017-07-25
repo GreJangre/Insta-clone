34637ea5dd8976dc9ea881dfa68622ca2a353c12
Jangre <dlatjdwn8824@naver.com>
2017-07-25 16:31:23 +0900

    js 추가

--- js/app.js
+++ js/app.js
@@ -1,6 +1,15 @@
 $(document).ready(function() {
-  var $heart = $(".fa-heart-o");
+  var $heart = $(".fa-heart-o"),
+    $ellipsis = $(".fa-ellipsis-h"),
+    $overlay = $(".overlay"),
+    $close = $(".fa-times");
   $heart.click(function() {
-    console.log("i got clicked");
+    $(this).toggleClass("fa-heart-o fa-heart heart-pumping");
   });
+  $ellipsis.click(function() {
+    $overlay.fadeIn(300);
+  });
+  $close.click(function() {
+    $overlay.fadeOut(300);
+  })
 });
